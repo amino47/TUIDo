95124cbe0bed96b882f0cf5fbfa8373776dde371
Amina Shellman <aminashellman@gmail.com>
2025-03-19 16:38:01 -0700

    fix build errors in main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header, Checkbox, Button
-from textual.containers import VerticalScroll, HorizonalGroup
+from textual.containers import HorizontalGroup, VerticalScroll
 from textual.reactive import reactive
 
 class TUIDo(App):
     """A Textual app to manage stopwatches."""
     CSS_PATH = "main.tcss"
     BINDINGS = [
-        ("d", "toggle_dark", "Toggle dark mode")
+        ("d", "toggle_dark", "Toggle dark mode"),
         ("a", "add_item", "Add a new item to the list"),            
     ]
 
-    class Item(HorizonalGroup):
+    class Item(HorizontalGroup):
         """A widget to represent an item in the list."""
 
         def compose(self) -> ComposeResult:
@@ -22,7 +22,7 @@ class TUIDo(App):
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
-        yield VerticalScroll(Item(), id="item_list")
+        yield VerticalScroll(self.Item(), id="item_list")
         yield Footer()
 
     def action_toggle_dark(self) -> None:
@@ -30,13 +30,9 @@ class TUIDo(App):
         self.theme = (
             "textual-dark" if self.theme == "textual-light" else "textual-light"
         )
-    def on_mount(self) -> None:
-        """Set theme to tokyo-night."""
-        self.theme = "tokyo-night"
-
     def action_add_item(self) -> None:
         """An action to add a new item to the list."""
-        self.root.append(Item())
+        self.VerticalScroll.append(self.Item())
 
 if __name__ == "__main__":
     app = TUIDo()
